a5934551381047df0790893999ac9b76ccc63611
Carlos Severo <carlos.severo@zipbank.com.br>
2024-12-03 10:22:00 -0300

    Add collectstatic to dockerfile, and new location to gitignore

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q83f@6i5*e!!x#w%q8wc=+bm%gbd-16!y&+%@#22q=7qc*k9yn'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
+else:
+    DEBUG = True
+    SECRET_KEY = 'django-insecure-q83f@6i5*e!!x#w%q8wc=+bm%gbd-16!y&+%@#22q=7qc*k9yn'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
